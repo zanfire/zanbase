a451b2da0ee4536b638f24502a5c9bcca6948dc2
Matteo Valdina <zanfire@gmail.com>
2012-08-01 22:16:01 +0200

    WIP

---
+++ include/zTest.h
@@ -0,0 +1,48 @@
+/******************************************************************************
+ * Copyright 2009 2012 Matteo Valdina
+ *      
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *      
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+
+#ifndef ZTEST_H__
+#define ZTEST_H__
+
+#include "zCommon.h"
+
+/// Test interface.
+/// 
+/// NOTE: Each test must be self-contained.
+/// NOTE: Each test can be executed at random order.
+/// @author Matteo Valdina
+class zTest {
+
+public:
+  zTest(void) {}
+  virtual ~zTest(void) {}
+
+  /// Execute the test at given index.
+  virtual bool execute(int index) = 0;
+  /// Returns the number of test implemented.
+  virtual int get_number_of_tests(void) = 0;
+  /// Returns the name of the Test implementation.
+  virtual char const* get_name(void) = 0;
+  /// Returns the description for the  Test implementation.
+  virtual char const* get_description(void) = 0;
+  /// Returns the test name at given index.
+  virtual char const* get_test_name(int index) = 0;
+  /// Returns the test description at given index.
+  virtual char const* get_test_description(int index) = 0;
+};
+
+#endif
+

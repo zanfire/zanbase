aabded1317149bb689ca57a1716304c72f3ad083
Matteo Valdina <zanfire@gmail.com>
2012-10-29 21:02:00 +0100

    - Added zColor.  - Clean-up.

---
+++ src/zColor.cpp
@@ -0,0 +1,13 @@
+#include "zColor.h"
+
+
+zSolidColor::zSolidColor(unsigned char r, unsigned char g, unsigned char b, unsigned char a) {
+  _r = r;
+  _g = g;
+  _b = b;
+  _a = a;
+}
+
+
+zSolidColor::~zSolidColor(void) {
+}
